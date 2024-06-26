91d0876fb03ee1f6aa86f6de6010181fd9d66272
NATACHAOS <164019299+NATACHAOS@users.noreply.github.com>
2024-06-26 16:46:48 +0300

    Add files via upload

--- iter.py
+++ iter.py
@@ -1,26 +1,29 @@
 class EvenNumbers:
     """Перебор чётных чисел в определённом числовом диапазоне"""
-    def __init__(self, start=0, end=1):
+
+    def __init__(self, start, end):
         self.start = start
         self.end = end
+        self.i = 0
 
     def __iter__(self):
-        self.start = self.start
-        self.result = self.start
+        self.i = 0
         return self
 
     def __next__(self):
-        if self.start > self.end:
-            raise StopIteration()
-        elif self.start % 2 == 0:
-            self.result = self.start
-            self.start += 1
-            return self.result
-        else:
-            self.start += 1
+        self.i += 1
+        if self.i >= self.start:
+            if self.i % 2 == 0:
+                return self.i
+            if self.i >= self.end:
+                raise StopIteration()
+            elif self.i / 2 != 0:
+                pass
+
+
+
 
 
 en = EvenNumbers(10, 25)
 for i in en:
-    if i != None:
-        print(i)
+    print(i)
